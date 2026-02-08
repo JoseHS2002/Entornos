18b03d51a984ae68331e5e5e35b51767494186e0
JoseHS <joshersou@alu.edu.gva.es>
2026-02-09 00:14:26 +0100

    Se crea el método principal.

--- src/ud5_3/CCuenta.java
+++ src/ud5_3/CCuenta.java
@@ -1,4 +1,20 @@
 package ud5_3;
 
 public class CCuenta {
+    public static void main(String[] args) {
+        // Depuracion. Se detiene siempre
+        CCuenta miCuenta = new CCuenta();
+        System.out.println("Saldo Inicial: "+miCuenta.dSaldo+" euros");
+        // Depuracion. Provoca parada por ingreso con cantidad menor de 0
+        miCuenta.ingresar(-100);
+        System.out.println("Saldo Inicial: "+miCuenta.dSaldo+" euros");
+        miCuenta.ingresar(100);
+        System.out.println("Saldo Inicial: "+miCuenta.dSaldo+" euros");
+        miCuenta.ingresar(200);
+        System.out.println("Saldo Inicial: "+miCuenta.dSaldo+" euros");
+        // Depuracion. Provoca parada con condicion de tercer ingreso
+        miCuenta.ingresar(300);
+        System.out.println("Saldo Inicial: "+miCuenta.dSaldo+" euros");
+
+    }
 }
