f40c4502da86320de5f33d7a153c7d366e4c02cd
JoseHS <joshersou@alu.edu.gva.es>
2026-02-10 20:20:54 +0100

    Creacion de tests

---
+++ test/ud5_3/CCuentaTest.java
@@ -0,0 +1,19 @@
+package ud5_3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CCuentaTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void ingresar() {
+    }
+
+    @Test
+    void retirar() {
+    }
+}
